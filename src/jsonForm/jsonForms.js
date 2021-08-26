673a350e630a5236fe451022a9d2825cc5626627
aliemrei <aliemreilhan@gmail.com>
2021-08-26 17:07:09 +0300

    1 file modified

--- src/jsonForm/jsonForms.js
+++ src/jsonForm/jsonForms.js
@@ -1,5 +1,4 @@
-
- import React from 'react';
+import React from 'react';
 import schema from './jsonFormSchema.json';
 import uischema from './jsonFormUISchema.json';
 import { JsonForms } from '@jsonforms/react';
