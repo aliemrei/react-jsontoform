979dc050319cd1972ae58a5863828ddb1df6cd57
aliemrei <aliemreilhan@gmail.com>
2021-08-26 17:20:22 +0300

    1 file modified

--- src/jsonForm/jsonForms.js
+++ src/jsonForm/jsonForms.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import { React, useState } from 'react';
 import schema from './jsonFormSchema.json';
 import uischema from './jsonFormUISchema.json';
 //import { JsonForms } from '@jsonforms/react';
@@ -7,60 +7,75 @@ import uischema from './jsonFormUISchema.json';
 //    materialRenderers,
 //  } from '@jsonforms/material-renderers';
 
-import { useState } from 'react';
-
 const initialData = {
-    name: 'Max Power',
-  };
+  name: 'Max Power'
+};
 
 function JsonForm() {
+  const onSubmit = formData => {
+    console.log('Data submitted: ', formData);
 
-    const onSubmit = (formData) => {
-        console.log("Data submitted: ", formData);
- 
-        var el = document.getElementById("formData");
+    var el = document.getElementById('formData');
 
-        if (el) {
-            el.value = JSON.stringify(formData, null, 2);
+    if (el) {
+      el.value = JSON.stringify(formData, null, 2);
+    }
+  };
 
-        }
+  var schemaText = JSON.stringify(schema, null, 2);
 
+  var uischemaText = JSON.stringify(uischema, null, 2);
 
-    };
+  const [data, setData] = useState(initialData);
 
-
-    var schemaText = JSON.stringify(schema, null, 2);
-
-    var uischemaText = JSON.stringify(uischema, null, 2);
-
-    const [data, setData] = useState(initialData);
-
-    return (
-        <div className="App container">
-            <div className="col-md-12">
-                <div className="col-md-6">
-                    <div class="h-100 p-5 bg-light border rounded-3">
-                        <h2>JSONSchema</h2>
-                        <textarea style={{ border: "0", readonly: "true", width: "100%", overflow: "auto", height: "400px" }} value={schemaText}  ></textarea>
-                    </div>
-                    <div class="h-100 p-5 bg-light border rounded-3">
-                        <h2>Data</h2>
-                        <textarea id="formData" style={{ border: "0", readonly: "true", width: "100%", overflow: "auto", height: "100px" }}    ></textarea>
-                    </div>
-                    <div class="h-100 p-5 bg-light border rounded-3">
-                        <h2>UI Schema</h2>
-                        <textarea style={{ border: "0", readonly: "true", width: "100%", overflow: "auto", height: "400px" }} value={uischemaText}  ></textarea>
-                    </div>
-
-                </div>
-                <div className="col-md-6">
-                  
-                </div>
-
-            </div>
+  return (
+    <div className="App container">
+      <div className="col-md-12">
+        <div className="col-md-6">
+          <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>JSONSchema</h2>
+            <textarea
+              style={{
+                border: '0',
+                readonly: 'true',
+                width: '100%',
+                overflow: 'auto',
+                height: '400px'
+              }}
+              value={schemaText}
+            />
+          </div>
+          <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>Data</h2>
+            <textarea
+              id="formData"
+              style={{
+                border: '0',
+                readonly: 'true',
+                width: '100%',
+                overflow: 'auto',
+                height: '100px'
+              }}
+            />
+          </div>
+          <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>UI Schema</h2>
+            <textarea
+              style={{
+                border: '0',
+                readonly: 'true',
+                width: '100%',
+                overflow: 'auto',
+                height: '400px'
+              }}
+              value={uischemaText}
+            />
+          </div>
         </div>
-    );
-
+        <div className="col-md-6" />
+      </div>
+    </div>
+  );
 }
 
 export default JsonForm;
