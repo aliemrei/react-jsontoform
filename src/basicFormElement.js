b498e96146e783d86cdbdc187196af9634bf5c8e
aliemrei <aliemreilhan@gmail.com>
2021-08-26 16:22:21 +0300

    1 file modified

--- src/basicFormElement.js
+++ src/basicFormElement.js
@@ -5,16 +5,17 @@ import { FormContext } from './FormContext';
 function BasicForm() {
   const [elements, setElements] = useState(null);
   useEffect(() => {
-    setElements(formJSON[0]);
-  }, []);
-  const { fields, page_label } = elements ?? {};
-  const handleSubmit = event => {
+    setElements(formJSON[0])
+
+  }, [])
+  const { fields, page_label } = elements ?? {}
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log(elements);
-  };
+    console.log(elements)
+  }
   const handleChange = (id, event) => {
-    const newElements = { ...elements };
+    const newElements = { ...elements }
     newElements.fields.forEach(field => {
       const { field_type, field_id } = field;
       if (id === field_id) {
@@ -27,27 +28,22 @@ function BasicForm() {
             field['field_value'] = event.target.value;
             break;
         }
+
+
       }
-      setElements(newElements);
+      setElements(newElements)
     });
-    console.log(elements);
-  };
+    console.log(elements)
+  }
   return (
     <FormContext.Provider value={{ handleChange }}>
       <div className="App container">
         <h3>{page_label}</h3>
         <form>
-          {fields
-            ? fields.map((field, i) => <Element key={i} field={field} />)
-            : null}
-          <button
-            type="submit"
-            className="btn btn-primary"
-            onClick={e => handleSubmit(e)}
-          >
-            Submit
-          </button>
+          {fields ? fields.map((field, i) => <Element key={i} field={field} />) : null}
+          <button type="submit" className="btn btn-primary" onClick={(e) => handleSubmit(e)}>Submit</button>
         </form>
+
       </div>
     </FormContext.Provider>
   );
