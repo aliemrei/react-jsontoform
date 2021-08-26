1abb52460e72895c6e233a33b4ef48c6e0e2ad4b
aliemrei <aliemreilhan@gmail.com>
2021-08-26 16:23:10 +0300

    2 files modified

--- src/basicFormElement.js
+++ src/basicFormElement.js
@@ -1,4 +1,5 @@
 import formJSON from './formElement.json';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import Element from './components/Element';
 import { FormContext } from './FormContext';
